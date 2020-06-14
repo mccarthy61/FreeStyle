258da9b5391976870425fee169777b4928291be9
Mike McCarthy <mike_mccarthy@hotmail.com>
2020-06-14 07:51:38 -0700

    Add files via upload

--- backend/build-and-run-docker-backend.bash
+++ backend/build-and-run-docker-backend.bash
@@ -1,28 +1,25 @@
 #!/bin/bash
-#
+
+
 # UCSC Extension DevOps
-# Homework 6
-# Michael McCarthy
+# Homework 7
 #
 # Build and Run Docker Backend
 #
 
 cd ~/UCSC/DevOps/d6/backend
 
-# docker images | findstr daemon
+sudo docker images | grep daemon
+
 # if %ERRORLEVEL% NEQ 0 (
-
-echo 'building backend docker image'
-docker build daemon -t daemon:latest
-
+#    echo 'building backend docker image'
+sudo docker build daemon -t daemon:latest
 # )
 
 echo '***** running backend docker image ******'
 dockernetwork="UCSCDevOpsMicroServices"
-echo $dockernetwork
+echo %dockernetwork%
 
-docker run --rm -it -d --name daemon -p 80:8000 --network $dockernetwork daemon:latest
-
-
+sudo docker run --rm -it -d --name daemon -p 80:8000 --network $dockernetwork daemon:latest
 
 
